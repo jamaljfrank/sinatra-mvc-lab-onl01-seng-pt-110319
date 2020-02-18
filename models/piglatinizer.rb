33c5cb8056e69bbb4ccfde72712449de8d64d78a
Jamal Frank <jamaljfrank@gmail.com>
2020-02-18 05:46:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
   end
 
   def piglatinize_word(word)
-    first_letter = word[0].downcase
+    word[0].downcase
       if ["a", "e", "i", "o", "u"].include?(first_letter)
         "#{word}way"
       else
