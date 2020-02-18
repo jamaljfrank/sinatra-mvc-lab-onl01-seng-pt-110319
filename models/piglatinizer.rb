bb6269c6c8af170ac62be59143d4bb2812bffe95
Jamal Frank <jamaljfrank@gmail.com>
2020-02-18 05:38:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,17 @@
 class PigLatinizer
 
-    def piglatinize(string)
-        a = string.split(" ")
-        b = a.map {|word| piglatinize_word(word)}
-        b.join(" ")
-    end
+  def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+  end
 
-    def piglatinize_word(word)
-        first_letter = word[0].downcase
-            if ["a", "e", "i", "o", "u"].include?(first_letter)
-                "#{word}way"
-            else
-                consonants = []
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+      if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+      else
+              consonants = []
                 consonants << word[0]
                 if ["a", "e", "i", "o", "u"].include?(word[1]) == false
                     consonants << word[1]
