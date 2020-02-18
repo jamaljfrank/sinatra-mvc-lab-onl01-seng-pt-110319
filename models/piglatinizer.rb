45f07176d39e30cd0df4ab33655c4571605d85fb
Jamal Frank <jamaljfrank@gmail.com>
2020-02-18 06:03:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
     @vowels = /[aeiou]/
     if word[0].downcase.start_with?(@vowels)
        "#{word}way"
-    elsif word[1].include?(@vowels)
+    elsif word[1].exclude?(@vowels)
       word[1..-1] + word[0] + "ay"
     end
   end
